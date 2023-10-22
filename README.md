2a25282db3f0f85ebf5058fd3eecf96d178ef5e0
phollaphat <phollaphat.s@ku.th>
2023-10-22 17:35:09 +0700

    lab10

---
+++ README.md
@@ -0,0 +1,4 @@
+# Shop Example using Cucumber
+
+[Cucumber presentation slide](https://github.com/ladyusa/cucumber-atm/blob/master/cucumber.pdf)
+# cucumber-shop
---
+++ src/test/java/ku/shop/BuyStepdefs.java
@@ -0,0 +1,48 @@
+package ku.shop;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BuyStepdefs {
+
+    private ProductCatalog catalog;
+    private Order order;
+
+    @Given("the store is ready to service customers")
+    public void the_store_is_ready_to_service_customers() {
+        catalog = new ProductCatalog();
+        order = new Order();
+    }
+
+    @Given("a product {string} with price {float} and stock of {int} exists")
+    public void a_product_exists(String name, double price, int stock) {
+        catalog.addProduct(name, price, stock);
+    }
+
+    @When("I buy {string} with quantity {int}")
+    public void i_buy_with_quantity(String name, int quantity) {
+        Product prod = catalog.getProduct(name);
+        order.addItem(prod, quantity);
+    }
+
+    @Then("total should be {float}")
+    public void total_should_be(double total) {
+        assertEquals(total, order.getTotal());
+    }
+
+
+    @Then("total in stock {string} quantity {int}")
+    public void total_in_stock_quantity(String name, int quantity) {
+        assertEquals(quantity, catalog.getProduct(name).getStock());
+    }
+}
+
+/**
+ ---------------------------------
+ // Phollaphat Soisermsup
+ // 6410450184
+ ---------------------------------
+ **/
